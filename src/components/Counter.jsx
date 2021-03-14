a57cd72160abeb80b3b57e70b6a45aa35e4c8991
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-03-14 15:05:44 -0300

    Conceito de  imutabilidade no React

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,18 @@
 import { useState } from 'react'
 
+// imutabilidade, criar um novo dado e não alterar um dado existente
+
+
+// usuarios = ['bruno', 'derek', 'bia']
+//usuarios.push('luiz')
+
+//add novo dado no array, cria uma novo e adiciona o novo item no final
+
+//novoUsuario = [... usuarios, 'Luiz']
+
 export function Counter() {
 
-    let [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(0)
 
     function increment() {
         console.log('increment')
