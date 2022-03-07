8f5fa7e85eb7739d19d80f83db179a1c87ebc772
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-03-07 14:16:39 -0300

    identação

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -27,13 +27,11 @@ export function RepositoryList() {
 
     return (
         <section className="repository-list">
-            <h1>Lista de respositórios</h1>
+            <h1>Lista de repositórios</h1>
             <ul>
                 {respositories.map(repository => (
                     <RepositoryItem key={repository.name} repository={repository} />
                 ))}
-
-
             </ul>
         </section>
     )
